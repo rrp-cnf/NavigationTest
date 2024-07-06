fb0ba49876df2ce6ae57bb1ac252aecece21eae3
Richie Rodríguez <ricardo.rodriguez@cinfo.es>
2024-07-06 12:37:25 +0200

    Initial Commit

---
+++ NavigationTest/NavigationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTestApp.swift
+//  NavigationTest
+//
+//  Created by Ricardo Rodríguez on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
